764514d1d1b9819d24b7e7aaa9e206e8f5225136
AarthiSaminathan <127826312+AarthiSaminathan@users.noreply.github.com>
2023-03-24 14:48:54 +0530

    Add files via upload

---
+++ Migrations/20230309051844_StudentDetailDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace ChangTrackingDemo.Migrations
+{
+    public partial class StudentDetailDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentDemo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentDemo", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentDemo");
+        }
+    }
+}
